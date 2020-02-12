8515259eeb685377903a3be03d684865f22912ab
rawiakart <42437943+rawiakart@users.noreply.github.com>
2020-02-13 00:20:13 +0100

    Revert "evennement"
    This reverts commit 6e4c4a9eddb9eaff69d8c1b35a0e122828ff59b2.

--- maysa/got/src/GOTBundle/GOTBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GOTBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class GOTBundle extends Bundle
-{
-}
--- maysa/got/src/GOTBundle/Tests/Controller/TemplateControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace GOTBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class TemplateControllerTest extends WebTestCase
-{
-}
